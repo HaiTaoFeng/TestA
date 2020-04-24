a5874c40f63f0884b7ab968c1a22017046c1ba55
fenghaitao <1213592271@qq.com>
2020-04-24 09:18:56 +0800

    更新

---
+++ README.md
@@ -0,0 +1,2 @@
+# TestA
+Android 将module中的jar、so包打包成jar或者aar文件提供给第三方使用的例子
---
+++ libraryb/src/main/java/com/example/libraryb/SysLoad.java
@@ -0,0 +1,12 @@
+package com.example.libraryb;
+
+/**
+ * @author fenghaitao
+ * @Time 2019年12月3日16:06:08
+ * @description
+ */
+public class SysLoad {
+    static {
+        System.loadLibrary("libAMapSDK_MAP_v7_1_0");
+    }
+}
